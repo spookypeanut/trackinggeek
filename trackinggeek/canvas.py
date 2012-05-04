4b4bc26287dd90a35262b314fe797db8cf526d8f
spookypeanut <github@spookypeanut.co.uk>
2012-05-04 07:17:46 +0100

    Get resolution, min, and max flags working correctly

--- trackinggeek/canvas.py
+++ trackinggeek/canvas.py
@@ -39,8 +39,10 @@ class Canvas(object):
         self.tracks = []
 
     def _calc_pixel_dimensions(self, pixel_dimensions):
-        if pixel_dimensions is None:
+        print ("Canvas._calc_pixel_dimensions(%s)" % (pixel_dimensions,))
+        if pixel_dimensions is None or len(pixel_dimensions.keys()) == 0:
             pixel_dimensions = {"max":DEFAULT_SIZE}
+        print pixel_dimensions
         if "width" in pixel_dimensions and "height" in pixel_dimensions:
             self.pixel_width = pixel_dimensions["width"]
             self.pixel_height = pixel_dimensions["height"]
@@ -49,27 +51,29 @@ class Canvas(object):
                             (self.max_latitude - self.min_latitude)
         if "width" in pixel_dimensions:
             self.pixel_width = pixel_dimensions["width"]
-            self.pixel_height = int(self.pixel_width / self.aspect_ratio)
+            self.pixel_height = int(float(self.pixel_width) / self.aspect_ratio)
             return
         if "height" in pixel_dimensions:
             self.pixel_height = pixel_dimensions["height"]
-            self.pixel_width = int(self.pixel_height * self.aspect_ratio)
+            self.pixel_width = int(float(self.pixel_height) * self.aspect_ratio)
             return
         if "max" in pixel_dimensions:
             if self.aspect_ratio > 1:
                 self.pixel_width = pixel_dimensions["max"]
-                self.pixel_height = int(self.pixel_width / self.aspect_ratio)
+                self.pixel_height = int(float(self.pixel_width) / self.aspect_ratio)
                 return
             self.pixel_height = pixel_dimensions["max"]
-            self.pixel_width = int(self.pixel_height * self.aspect_ratio)
+            print(self.pixel_height)
+            print(self.pixel_height)
+            self.pixel_width = int(float(self.pixel_height) * self.aspect_ratio)
             return
         if "min" in pixel_dimensions:
             if self.aspect_ratio < 1:
                 self.pixel_width = pixel_dimensions["min"]
-                self.pixel_height = int(self.pixel_width / self.aspect_ratio)
+                self.pixel_height = int(float(self.pixel_width) / self.aspect_ratio)
                 return
             self.pixel_height = pixel_dimensions["min"]
-            self.pixel_width = int(self.pixel_height * self.aspect_ratio)
+            self.pixel_width = int(float(self.pixel_height) * self.aspect_ratio)
             return
         print pixel_dimensions
         raise ValueError("Could not calculate the image resolution")
@@ -153,10 +157,10 @@ class Canvas(object):
     def draw(self):
         self._calc_pixel_dimensions(self.pixel_dimensions)
         self.surface = cairo.SVGSurface("/tmp/test.svg",
-                                        self.pixel_width,
-                                        self.pixel_height)
+                                        float(self.pixel_width),
+                                        float(self.pixel_height))
         self.ctx = cairo.Context(self.surface)
-        self.ctx.scale (self.pixel_width, self.pixel_height)
+        self.ctx.scale (float(self.pixel_width), float(self.pixel_height))
 
         for track in self.tracks:
             self._draw_track(track)
