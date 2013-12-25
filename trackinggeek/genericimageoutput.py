52deffeff293dd4613038591962c43dcac9e9eba
spookypeanut <github@spookypeanut.co.uk>
2013-12-25 23:52:40 +0000

    Back to where I was, but with easier to handle codebase
    * Tracks should be cumulative
    * Need to auto-choose timelapse vs single image

--- trackinggeek/genericimageoutput.py
+++ trackinggeek/genericimageoutput.py
@@ -14,8 +14,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import os
-
 from trackinggeek.track import Track
 from trackinggeek.util import mercator_adjust, tracks_from_path
 
--- trackinggeek/timelapse.py
+++ trackinggeek/timelapse.py
@@ -15,5 +15,81 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from trackinggeek.genericimageoutput import GenericImageOutput
+from trackinggeek.canvas import Canvas
+from trackinggeek.util import add_num_to_path
+
 class Timelapse(GenericImageOutput):
-    pass
+    def __init__(self, latitude_range=None, longitude_range=None,
+                 pixel_dimensions=None, config=None):
+        GenericImageOutput.__init__(self, latitude_range=latitude_range,
+                                    longitude_range=longitude_range,
+                                    pixel_dimensions=pixel_dimensions,
+                                    config=config)
+        self._frames_prepared = False
+        self.timelapse_unit = config.get_timelapse_unit()
+        self.units_per_frame = config.get_units_per_frame()
+
+    def draw(self):
+        self.prepare_to_draw()
+        self.resolution = (self.pixel_width, self.pixel_height)
+        self.latitude_range = (self.min_latitude, self.max_latitude)
+        self.longitude_range = (self.min_longitude, self.max_longitude)
+        self.speed_range = (self.min_speed, self.max_speed)
+        self.elevation_range = (self.min_elevation, self.max_elevation)
+        self._prepare_frames()
+        for f in self.frames:
+            f.draw()
+
+    def save_png(self, path):
+        """ Save the canvas as a png file
+        """
+        for f in self.frames:
+            num_path = add_num_to_path(path, f.frame_number)
+            f.canvas.surface.write_to_png(num_path)
+
+    def save_svg(self, path):
+        #self.surface.finish()
+        raise NotImplementedError
+
+    def _get_canvas(self, frame_number):
+        return Canvas(resolution=self.resolution,
+                      latitude_range=self.latitude_range,
+                      longitude_range=self.longitude_range,
+                      speed_range=self.speed_range,
+                      elevation_range=self.elevation_range,
+                      config=self.config)
+
+    def _prepare_frames(self):
+        if self._frames_prepared:
+            return
+        self.frames = []
+        # TODO: We might want to do some cleverer sorting
+        self.tracks.sort()
+        if self.timelapse_unit == "track":
+            batchsize = self.units_per_frame
+            counter = 1
+            # This splits a list into batches, ie
+            # [1,2,3,4,5] -> [[1,2],[3,4],[5]]
+            for tracklist in [self.tracks[i:i+batchsize] for i in 
+                                range(0, len(self.tracks), batchsize)]:
+                print("Doing frame %s" % counter)
+                print("tracklist = %s" % (tracklist,))
+                canvas = self._get_canvas(counter)
+                self.frames.append(Frame(canvas=canvas, tracks=tracklist,
+                                         frame_number=counter))
+                counter += 1
+        else:
+            raise NotImplementedError("Don't know how to handle %s" %
+                    self.timelapse_unit)
+        self._frames_prepared = True
+
+class Frame(object):
+    def __init__(self, canvas, tracks, frame_number):
+        self.canvas = canvas
+        # Let's keep this. We might want to postpone the track parsing
+        # in future
+        self.tracks = tracks
+        self.frame_number = frame_number
+
+    def draw(self):
+        self.canvas.draw_tracks(self.tracks)
